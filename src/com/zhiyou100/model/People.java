d53dbc322f786df592b2a15232667bbe938fe2c3
yu <1502303824@qq.com>
2018-01-05 18:51:48 +0800

    新增一个People类

---
+++ src/com/zhiyou100/model/People.java
@@ -0,0 +1,18 @@
+package com.zhiyou100.model;
+
+
+/**  
+* @ClassName: People  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午6:50:39
+*  
+*/
+
+public class People {
+
+	
+	
+	
+	
+}
---
+++ src/com/zhiyou100/model/User.java
@@ -0,0 +1,19 @@
+package com.zhiyou100.model;
+
+
+/**  
+* @ClassName: User  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午6:45:43
+*  
+*/
+
+public class User {
+	private String name;
+	
+	
+	
+	
+
+}
