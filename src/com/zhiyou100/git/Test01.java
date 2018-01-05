f61c4f309cdd00288cf3bceda68939de101f87d9
yu <34875641+yu986921845@users.noreply.github.com>
2018-01-05 21:03:49 +0800

    Revert "LM 1"

--- src/com/zhiyou100/git/Test01.java
+++ src/com/zhiyou100/git/Test01.java
@@ -14,8 +14,6 @@ public class Test01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("添加文件01");
-         System.out.println("添加文件01");
-         System.out.println("添加文件01");
 	}
 
 }
